7692da422b3d2549fa25d6de75465427d73597b4
JaiminBariya <jaiminbariyastudent@gmail.com>
2025-02-14 11:38:19 +0530

    chg: solve typo in congig/settings.py and commnet .env for templating

--- Backend - Flask/config/settings.py
+++ Backend - Flask/config/settings.py
@@ -4,6 +4,6 @@ import os
 
 class Config:
     """Base configuration (default for all environments)"""
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", 'qlite:///database.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", 'sqlite:///database.db')
     SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
